b789c310b18b3aa47d3ddeb230ea0ba6162efa73
Emilie <emilie.dannenberg@gmail.com>
2016-10-13 12:33:18 +0100

    add constant

--- index.android.js
+++ index.android.js
@@ -13,6 +13,10 @@ import {
   View,
 } from 'react-native';
 
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
+
 export default class reactTutorial extends Component {
   render() {
     return (
